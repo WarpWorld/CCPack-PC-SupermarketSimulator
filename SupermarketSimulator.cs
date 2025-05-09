621314426d52f05861c7a614dba1cefba0c407db
Crowd Control Effect Pack Synchronization Tool <kat@warp.world>
2025-05-09 20:11:18 +0000

    Crowd Control Effect Pack Synchronization Tool - Automated Commit

--- SupermarketSimulator.cs
+++ SupermarketSimulator.cs
@@ -10,7 +10,7 @@ public class SupermarketSimulator : SimpleTCPPack<SimpleTCPServerConnector>
 
     public override ushort Port => 51337;
 
-    public override ISimpleTCPPack.MessageFormat MessageFormat => ISimpleTCPPack.MessageFormat.CrowdControlLegacy;
+    public override ISimpleTCPPack.MessageFormatType MessageFormat => ISimpleTCPPack.MessageFormatType.CrowdControlLegacy;
 
     public SupermarketSimulator(UserRecord player, Func<CrowdControlBlock, bool> responseHandler, Action<object> statusUpdateHandler) : base(player, responseHandler, statusUpdateHandler) { }
 
